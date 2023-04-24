0d8ed2802dae4671d727586683a15d1b57521a99
Mark Janssen-vooles <m.janssen-vooles@outlook.com.au>
2023-04-24 14:08:33 +1000

    Add new title and flavicon emoji

--- src/script.js
+++ src/script.js
@@ -4,8 +4,6 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js'
 import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js'
 import * as dat from 'lil-gui'
-import { Mesh, Raycaster } from 'three'
-import { generateUUID } from 'three/src/math/MathUtils'
 
 /**
  * Base
