43bf84273a6d289176c82cd6b171baf896d5180a
Mark Janssen-vooles <m.janssen-vooles@outlook.com.au>
2023-09-13 15:41:58 +1000

    format js

--- src/script.js
+++ src/script.js
@@ -3,47 +3,42 @@ import { ipadAnimation } from "./animations/ipad-animation";
 import { mobileAnimation } from "./animations/mobile-animation";
 
 const resetCanvas = () => {
-	const newCanvas = document.createElement("canvas")
-	newCanvas.className = "webgl";
-	newCanvas.style.zIndex = "-1";
-	const canvas = document.querySelector("canvas.webgl");
-	canvas.remove();
-	document.querySelector("body").appendChild(newCanvas);
-}
+  const newCanvas = document.createElement("canvas");
+  newCanvas.className = "webgl";
+  newCanvas.style.zIndex = "-1";
+  const canvas = document.querySelector("canvas.webgl");
+  canvas.remove();
+  document.querySelector("body").appendChild(newCanvas);
+};
 
 const renderDesktopAnimation = () => {
-  console.log("render for desktop");
-	resetCanvas()
-	desktopAnimation();  
-}
+  resetCanvas();
+  desktopAnimation();
+};
 
 const renderIpadAnimation = () => {
-	console.log("render for ipad");  
-	resetCanvas()
-	ipadAnimation()
-}
+  resetCanvas();
+  ipadAnimation();
+};
 
 const renderMobileAnimation = () => {
-  console.log("render for mobile");  
-	resetCanvas()
-	mobileAnimation()
-}
+  resetCanvas();
+  mobileAnimation();
+};
 
-
-// initial render 
-if (window.innerWidth >= 1280) 
-  renderDesktopAnimation()
-if (window.innerWidth < 1280 && window.innerWidth >= 820)
-	renderIpadAnimation()
-if (window.innerWidth < 820)
-	renderMobileAnimation()
+// initial render
+if (window.innerWidth >= 1280) renderDesktopAnimation();
+if (window.innerWidth < 1280 && window.innerWidth >= 820) renderIpadAnimation();
+if (window.innerWidth < 820) renderMobileAnimation();
 
 // event listener for screen size
-window.addEventListener("resize", (event) => {
-  if (window.innerWidth >= 1280) 
-		renderDesktopAnimation()
-	if (window.innerWidth < 1280 && window.innerWidth >= 820)
-		renderIpadAnimation()
-	if (window.innerWidth < 820)
-		renderMobileAnimation()
-}, true);
+window.addEventListener(
+  "resize",
+  (_) => {
+    if (window.innerWidth >= 1280) renderDesktopAnimation();
+    if (window.innerWidth < 1280 && window.innerWidth >= 820)
+      renderIpadAnimation();
+    if (window.innerWidth < 820) renderMobileAnimation();
+  },
+  true
+);
